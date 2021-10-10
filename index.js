b243f6bd7b849486b6858106e3c210c3d1a6952e
Javo2205 <javo.delara@gmail.com>
2021-10-10 13:03:51 -0500

    prueba terminada

---
+++ index.js
@@ -0,0 +1,24 @@
+function openSlideMenu(){
+    document.getElementById('side-nav').style.width = '80%';
+  }
+
+function closeSlideMenu(){
+  document.getElementById('side-nav').style.width = '0';
+}
+
+function openTicketsTime(){
+  document.getElementById('tickets-time').style.height = '80%';
+}
+
+function closeTicketsTime(){
+  document.getElementById('tickets-time').style.height = '0';
+}
+
+function openWatchTrailer(){
+  document.getElementById('watch-trailer').style.height = '80%';
+}
+
+function closeWatchTrailer(){
+  document.getElementById('watch-trailer').style.height = '0';
+}
+
